8e992df398dd8f966346503852ae9882138ae8b1
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-12 09:51:09 -0400

    Trying to add slides to the home page

---
+++ script.js
@@ -0,0 +1,10 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,1000);
+
+function nextSlide() {
+    console.log(slides);
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
